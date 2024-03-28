3988396d61bd475095820ea4711c27a82b1b9402
Altynbekow <152465429+Altynbekow@users.noreply.github.com>
2024-03-28 10:11:20 +0600

    new object

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,31 +1,21 @@
 import cls from './sideBar.module.css'
 import logo from '../../assets/logo.svg'
-import explor from '../../assets/Explore.svg'
-import icon from '../../assets/Icon.svg'
-import vector from '../../assets/Vector.svg'
+import explor from '../../assets/explor.svg'
+import icon from '../../assets/settings.svg'
+import vector from '../../assets/cities.svg'
 import weather from '../../assets/weather.svg'
 
 const SideBar = () => {
     return (
         <div className={cls.sideBar}>
-            <div className={cls.side__top}>
-                <img src={logo} alt=""/>
+            <div>
+                <img src={logo} alt="" />
             </div>
-            <div className={cls.side__nav}>
-                <img src={weather} alt=""/>
-                <p>weather</p>
-            </div>
-            <div className={cls.side__explor}>
-                <img className={cls.side__explor} src={explor} alt=""/>
-                <p className={cls.side__explori}>explore</p>
-            </div>
-            <div className={cls.side}>
-                <img className={cls.side__explor} src={vector} alt=""/>
-                <p className={cls.side__explori}>cities</p>
-            </div>
-            <div className={cls.side}>
-                <img className={cls.side__explor} src={icon} alt=""/>
-                <p className={cls.side__explori}>settings</p>
+            <div className={cls.menu}>
+                <img className={cls.icmenu} src={weather} alt="" />
+                <img className={cls.icmenu} src={explor} alt="" />
+                <img className={cls.icmenu} src={vector} alt="" />
+                <img className={cls.icmenu} src={icon} alt="" />
             </div>
         </div>
     );
